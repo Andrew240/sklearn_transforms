939245d6bdf5a709edcde1617b48077ab0b9fd23
Andrew240 <momo7153@gmail.com>
2020-08-26 21:28:48 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,25 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+ 
+class DataFrameImputer(TransformerMixin):
+
+    def __init__(self):
+        """Remplaza los valores perdidos.
+
+        Las columnas del tipo de objeto se sustituyen con el valor más frecuente 
+        en la columna.
+
+        Las columnas de otros tipos se sustituyen con la media de la columna.
+
+        """
+    def fit(self, X, y=None):
+
+        self.fill = pd.Series([X[c].value_counts().index[0]
+            if X[c].dtype == np.dtype('O') else X[c].mean() for c in X],
+            index=X.columns)
+
+        return self
+
+    def transform(self, X, y=None):
+        return X.fillna(self.fill)
